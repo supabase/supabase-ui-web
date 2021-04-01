50fd90b114a44c02d935f0972e65087513da7e68
Jonathan Summers-Muir <jon.summers.muir@googlemail.com>
2021-04-01 20:12:17 +0800

    add warning on the front

--- pages/components/[slug].tsx
+++ pages/components/[slug].tsx
@@ -44,7 +44,7 @@ export default function Home({ source, frontmatter, toc }: any) {
 
   return (
     <DefaultLayout>
-      <div className="grid grid-cols-12 container px-0 lg:py-8 mx-auto lg:gap-8 ">
+      <div className="grid grid-cols-12 container px-0 lg:py-8 mx-auto lg:gap-16">
         <article className="col-span-12 lg:col-span-9 xl:col-span-7">
           <Typography.Title>{frontmatter.title}</Typography.Title>
           <Typography.Title level={3}>
