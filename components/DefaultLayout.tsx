469383c5be958d9a6e492351432f33af1e84d452
Jonathan Summers-Muir <jon.summers.muir@googlemail.com>
2021-04-01 18:00:33 +0800

    add mobile menu

--- components/DefaultLayout.tsx
+++ components/DefaultLayout.tsx
@@ -16,6 +16,7 @@ import { useEffect, useState } from "react";
 
 function DefaultLayout(props: any) {
   const [darkMode, setDarkMode] = useState<boolean>(true);
+  const [showMobileMenu, setShowMobileMenu] = useState<boolean>(false);
 
   useEffect(() => {
     const isDarkMode = localStorage.getItem("supabaseDarkMode");
@@ -48,6 +49,77 @@ function DefaultLayout(props: any) {
     );
   }
 
+  const contents = (
+    <>
+      <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
+        <div className="flex items-center flex-shrink-0 px-4">
+          {/* <img
+        className="h-8 w-auto"
+        src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"
+        alt="Workflow"
+      /> */}
+          <Link href="/" as="/">
+            <img
+              className="block h-6 w-auto"
+              src={
+                darkMode ? `/images/logo-dark.png` : `/images/logo-light.png`
+              }
+              alt="Logo"
+            />
+          </Link>
+        </div>
+        <div className="ml-3 mt-6">
+          <Badge>Pre release alpha</Badge>
+        </div>
+        <nav className="pt-6 flex-1 bg-white dark:bg-gray-800 space-y-1">
+          {/* <!-- Current: "bg-gray-100 text-gray-900", Default: "text-gray-600 hover:bg-gray-50 hover:text-gray-900" --> */}
+
+          <Menu>
+            <Link href="/">
+              <Menu.Item>Introduction</Menu.Item>
+            </Link>
+            <Menu.Item>Installation (to do)</Menu.Item>
+            <Menu.Item>Dark mode (to do)</Menu.Item>
+            <Menu.Item>License (to do)</Menu.Item>
+
+            {Object.values(MenuData).map((menu: any, i) => {
+              // return <span>{menu}</span>;
+              const title = Object.keys(MenuData)[i];
+              return (
+                <>
+                  <div className="mt-4">
+                    <Menu.Group title={title} />
+                  </div>
+                  {menu.map((menu: any) => {
+                    return (
+                      <Link
+                        href={`/components/${menu.toLowerCase()}`}
+                        as={`/components/${menu.toLowerCase()}`}
+                      >
+                        <a>
+                          <Menu.Item>{menu}</Menu.Item>
+                        </a>
+                      </Link>
+                    );
+                  })}
+                </>
+              );
+            })}
+          </Menu>
+        </nav>
+      </div>
+      <div className=" border-t dark:border-gray-600">
+        <div className="">
+          <Menu>
+            <Menu.Item>
+              <DarkModeToggle />
+            </Menu.Item>
+          </Menu>
+        </div>
+      </div>
+    </>
+  );
+
   return (
     // <div className="container">
     //   <div>
@@ -57,106 +129,27 @@ function DefaultLayout(props: any) {
     // </div>
     // <!-- This example requires Tailwind CSS v2.0+ -->
     <div className="h-screen flex overflow-hidden bg-white dark:bg-gray-800">
-      {/* <Nav /> */}
+      <Nav
+        contents={contents}
+        show={showMobileMenu}
+        setShow={() => setShowMobileMenu(!showMobileMenu)}
+      />
 
       {/* <!-- Static sidebar for desktop --> */}
       <div className="hidden md:flex md:flex-shrink-0">
         <div className="flex flex-col w-60">
           {/* <!-- Sidebar component, swap this element with another sidebar if you like --> */}
           <div className="flex flex-col h-0 flex-1 border-r border-gray-200 dark:border-gray-600 bg-white dark:bg-gray-800">
-            <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
-              <div className="flex items-center flex-shrink-0 px-4">
-                {/* <img
-                  className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"
-                  alt="Workflow"
-                /> */}
-                <Link href="/" as="/">
-                  <img
-                    className="block h-6 w-auto"
-                    src={
-                      darkMode
-                        ? `/images/logo-dark.png`
-                        : `/images/logo-light.png`
-                    }
-                    alt="Logo"
-                  />
-                </Link>
-              </div>
-              <div className="ml-3 mt-6">
-                <Badge>Pre release alpha</Badge>
-              </div>
-              <nav className="pt-6 flex-1 bg-white dark:bg-gray-800 space-y-1">
-                {/* <!-- Current: "bg-gray-100 text-gray-900", Default: "text-gray-600 hover:bg-gray-50 hover:text-gray-900" --> */}
-
-                <Menu>
-                  <Link href="/">
-                    <Menu.Item>Introduction</Menu.Item>
-                  </Link>
-                  <Menu.Item>Installation (to do)</Menu.Item>
-                  <Menu.Item>Dark mode (to do)</Menu.Item>
-                  <Menu.Item>License (to do)</Menu.Item>
-
-                  {Object.values(MenuData).map((menu: any, i) => {
-                    // return <span>{menu}</span>;
-                    const title = Object.keys(MenuData)[i];
-                    return (
-                      <>
-                        <div className="mt-4">
-                          <Menu.Group title={title} />
-                        </div>
-                        {menu.map((menu: any) => {
-                          return (
-                            <Link
-                              href={`/components/${menu.toLowerCase()}`}
-                              as={`/components/${menu.toLowerCase()}`}
-                            >
-                              <a>
-                                <Menu.Item>{menu}</Menu.Item>
-                              </a>
-                            </Link>
-                          );
-                        })}
-                      </>
-                    );
-                  })}
-                </Menu>
-              </nav>
-            </div>
-            <div className=" border-t dark:border-gray-600">
-              <div className="">
-                <Menu>
-                  <Menu.Item>
-                    <DarkModeToggle />
-                  </Menu.Item>
-                </Menu>
-              </div>
-              {/* <a href="#" className="flex-shrink-0 w-full group block">
-                <div className="flex items-center">
-                  <div>
-                    <img
-                      className="inline-block h-9 w-9 rounded-full"
-                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixqx=Dy8dXEWwmB&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                      alt=""
-                    />
-                  </div>
-                  <div className="ml-3">
-                    <p className="text-sm font-medium text-gray-700 group-hover:text-gray-900">
-                      Tom Cook
-                    </p>
-                    <p className="text-xs font-medium text-gray-500 group-hover:text-gray-700">
-                      View profile
-                    </p>
-                  </div>
-                </div>
-              </a> */}
-            </div>
+            {contents}
           </div>
         </div>
       </div>
       <div className="flex flex-col w-0 flex-1 overflow-hidden">
         <div className="md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3">
-          <button className="-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+          <button
+            onClick={() => setShowMobileMenu(!showMobileMenu)}
+            className="-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+          >
             <span className="sr-only">Open sidebar</span>
             {/* <!-- Heroicon name: outline/menu --> */}
             <svg
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,215 +1,51 @@
-function Nav() {
+import { Transition } from "@supabase/ui";
+import { getStaticProps } from "~/pages";
+
+function Nav({ contents, show, setShow }: any) {
   return (
-    <div
-      className="fixed inset-0 flex z-40 md:hidden"
-      role="dialog"
-      aria-modal="true"
-    >
-      <div
-        className="fixed inset-0 bg-gray-600 bg-opacity-75"
-        aria-hidden="true"
-      ></div>
-
-      <div className="relative flex-1 flex flex-col max-w-xs w-full bg-white">
-        <div className="absolute top-0 right-0 -mr-12 pt-2">
-          <button className="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-            <span className="sr-only">Close sidebar</span>
-            {/* <!-- Heroicon name: outline/x --> */}
-            <svg
-              className="h-6 w-6 text-white"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </button>
-        </div>
-
-        <div className="flex-1 h-0 pt-5 pb-4 overflow-y-auto">
-          <div className="flex-shrink-0 flex items-center px-4">
-            <img
-              className="h-8 w-auto"
-              src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"
-              alt="Workflow"
-            />
+    <>
+      <Transition
+        show={show}
+        enter="transition-opacity duration-75"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="transition-opacity duration-150"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+        <div
+          onClick={setShow}
+          className="z-30 fixed inset-0 bg-gray-600 bg-opacity-75"
+          aria-hidden="true"
+        ></div>
+      </Transition>
+      <Transition
+        show={show}
+        enter="transform transition ease-in-out duration-500 sm:duration-700"
+        enterFrom="-translate-x-full"
+        enterTo="translate-x-0"
+        leave="transform transition ease-in-out duration-500 sm:duration-700"
+        leaveFrom="translate-x-0"
+        leaveTo="-translate-x-full"
+      >
+        <div
+          className="fixed inset-0 flex z-40 md:hidden"
+          role="dialog"
+          aria-modal="true"
+        >
+          {/* // capture close clicks */}
+          <div className="absolute h-full w-full" onClick={setShow}></div>
+          {/* content of mobile menu */}
+          <div className="relative flex-1 flex flex-col max-w-xs w-full bg-white dark:bg-gray-800">
+            {contents}
           </div>
-          <nav className="mt-5 px-2 space-y-1">
-            {/* <!-- Current: "bg-gray-100 text-gray-900", Default: "text-gray-600 hover:bg-gray-50 hover:text-gray-900" --> */}
-            <a
-              href="#"
-              className="bg-gray-100 text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md"
-            >
-              {/* <!-- Current: "text-gray-500", Default: "text-gray-400 group-hover:text-gray-500" --> */}
-              {/* <!-- Heroicon name: outline/home --> */}
-              <svg
-                className="text-gray-500 mr-4 h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                />
-              </svg>
-              Dashboard
-            </a>
 
-            <a
-              href="#"
-              className="text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md"
-            >
-              {/* <!-- Heroicon name: outline/users --> */}
-              <svg
-                className="text-gray-400 group-hover:text-gray-500 mr-4 h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
-                />
-              </svg>
-              Team
-            </a>
-
-            <a
-              href="#"
-              className="text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md"
-            >
-              {/* <!-- Heroicon name: outline/folder --> */}
-              <svg
-                className="text-gray-400 group-hover:text-gray-500 mr-4 h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
-                />
-              </svg>
-              Projects
-            </a>
-
-            <a
-              href="#"
-              className="text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md"
-            >
-              {/* <!-- Heroicon name: outline/calendar --> */}
-              <svg
-                className="text-gray-400 group-hover:text-gray-500 mr-4 h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-                />
-              </svg>
-              Calendar
-            </a>
-
-            <a
-              href="#"
-              className="text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md"
-            >
-              {/* <!-- Heroicon name: outline/inbox --> */}
-              <svg
-                className="text-gray-400 group-hover:text-gray-500 mr-4 h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"
-                />
-              </svg>
-              Documents
-            </a>
-
-            <a
-              href="#"
-              className="text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md"
-            >
-              {/* <!-- Heroicon name: outline/chart-bar --> */}
-              <svg
-                className="text-gray-400 group-hover:text-gray-500 mr-4 h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-                />
-              </svg>
-              Reports
-            </a>
-          </nav>
+          <div className="flex-shrink-0 w-14">
+            {/* <!-- Force sidebar to shrink to fit close icon --> */}
+          </div>
         </div>
-        <div className="flex-shrink-0 flex border-t border-gray-200 p-4">
-          <a href="#" className="flex-shrink-0 group block">
-            <div className="flex items-center">
-              <div>
-                <img
-                  className="inline-block h-10 w-10 rounded-full"
-                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixqx=Dy8dXEWwmB&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt=""
-                />
-              </div>
-              <div className="ml-3">
-                <p className="text-base font-medium text-gray-700 group-hover:text-gray-900">
-                  Tom Cook
-                </p>
-                <p className="text-sm font-medium text-gray-500 group-hover:text-gray-700">
-                  View profile
-                </p>
-              </div>
-            </div>
-          </a>
-        </div>
-      </div>
-
-      <div className="flex-shrink-0 w-14">
-        {/* <!-- Force sidebar to shrink to fit close icon --> */}
-      </div>
-    </div>
+      </Transition>
+    </>
   );
 }
 
