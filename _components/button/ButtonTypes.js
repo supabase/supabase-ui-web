a4a3fc0d8766baef09beecebf02d5dc1cba962ac
Emerson Laurentino <emerson@laurentino.co>
2021-04-02 16:10:50 -0300

    fix(ButtonTypes): default button with correct text

--- _components/button/ButtonTypes.js
+++ _components/button/ButtonTypes.js
@@ -4,7 +4,7 @@ export default function ButtonTypes() {
   return (
     <>
       <Button type="primary">Primary Button</Button>
-      <Button type="default">Primary Button</Button>
+      <Button type="default">Default Button</Button>
       <Button type="dashed">Dashed Button</Button>
       <Button type="outline">Outline Button</Button>
       <Button type="text">Text Button</Button>
