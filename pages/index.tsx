4b7ce154b99f00bf391b19d6659414a6e5013485
Jonathan Summers-Muir <jon.summers.muir@googlemail.com>
2021-04-09 19:31:43 +0800

    test push

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,35 @@
-import fs from "fs";
-import path from "path";
-import matter from "gray-matter";
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
 
-import renderToString from "next-mdx-remote/render-to-string";
-import hydrate from "next-mdx-remote/hydrate";
+import renderToString from 'next-mdx-remote/render-to-string'
+import hydrate from 'next-mdx-remote/hydrate'
 
-import DefaultLayout from "../components/DefaultLayout";
+import DefaultLayout from '../components/DefaultLayout'
 
-import { Alert, Typography } from "@supabase/ui";
-import ReactMarkdown from "react-markdown";
+import { Alert, Typography } from '@supabase/ui'
+import ReactMarkdown from 'react-markdown'
 
-const components = { Alert };
+const components = { Alert }
 
 export default function Home({ source, frontmatter, toc }: any) {
-  const gfm = require("remark-gfm");
+  const gfm = require('remark-gfm')
 
-  const content = hydrate(source, { components });
+  const content = hydrate(source, { components })
 
   const TableOfContents = toc && (
     <div className="toc">
       <Typography.Title level={5}>Contents</Typography.Title>
       <ReactMarkdown plugins={[gfm]}>{toc.content}</ReactMarkdown>
     </div>
-  );
+  )
 
   return (
     <DefaultLayout>
       <div className="grid grid-cols-12 container px-0 lg:py-8 mx-auto lg:gap-16">
         <article className="col-span-12 lg:col-span-9 xl:col-span-7">
           <Typography.Title>{frontmatter.title}</Typography.Title>
-          <Typography.Title level={3}>
-            {frontmatter.description}
-          </Typography.Title>
+          <Typography.Title level={3}>{frontmatter.description}</Typography.Title>
           <Typography>{content}</Typography>
         </article>
         <div className="hidden lg:block lg:col-span-3 xl:col-span-2 col-start-11">
@@ -39,23 +37,23 @@ export default function Home({ source, frontmatter, toc }: any) {
         </div>
       </div>
     </DefaultLayout>
-  );
+  )
 }
 
 export async function getStaticProps() {
   // plugins for next-mdx-remote
-  const gfm = require("remark-gfm");
-  const slug = require("rehype-slug");
+  const gfm = require('remark-gfm')
+  const slug = require('rehype-slug')
 
   // table of contents extractor
-  const toc = require("markdown-toc");
+  const toc = require('markdown-toc')
 
   //Finding directory named "blog" from the current working directory of Node.
-  const filePath = `Introduction`;
-  const postDirectory = path.join(process.cwd(), "_posts");
-  const fullPath = path.join(postDirectory, `${filePath}.mdx`);
-  const fileContent = fs.readFileSync(fullPath, "utf8");
-  const { data, content } = matter(fileContent);
+  const filePath = `Introduction`
+  const postDirectory = path.join(process.cwd(), '_posts')
+  const fullPath = path.join(postDirectory, `${filePath}.mdx`)
+  const fileContent = fs.readFileSync(fullPath, 'utf8')
+  const { data, content } = matter(fileContent)
 
   const mdxSource: any = await renderToString(content, {
     components,
@@ -64,7 +62,7 @@ export async function getStaticProps() {
       remarkPlugins: [gfm],
       rehypePlugins: [slug],
     },
-  });
+  })
 
   return {
     props: {
@@ -74,5 +72,5 @@ export async function getStaticProps() {
       },
       toc: toc(content, { maxdepth: 3 }),
     },
-  };
+  }
 }
