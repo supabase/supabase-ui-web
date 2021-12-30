9c1629614141da34a0032f0ea9d037b34b6faa76
pip <jrphiri@gmail.com>
2021-12-30 03:37:25 +0200

    docs/icon: adds Icon documentation

---
+++ _components/icon/IconSize.js
@@ -0,0 +1,13 @@
+import { IconMail } from '@supabase/ui'
+
+export default function IconSizes() {
+  return (
+    <>
+      <IconMail size={12} />
+      <IconMail size={16} />
+      <IconMail size={18} />
+      <IconMail size={22} />
+      <IconMail size={48} />
+    </>
+  )
+}
