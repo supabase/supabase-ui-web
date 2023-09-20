b7abbdbb14d50602359ae904e424bff74e01e5d2
Joshen Lim <joshenlimek@gmail.com>
2023-09-20 21:00:04 +0800

    Add redirect to auth UI page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
-const withMDX = require("@next/mdx")();
+const withMDX = require('@next/mdx')()
 
 module.exports = withMDX({
   //   basePath: "/new",
-  pageExtensions: ["js", "jsx", "tsx", "md", "mdx"],
-});
+  pageExtensions: ['js', 'jsx', 'tsx', 'md', 'mdx'],
+  async redirects() {
+    return [
+      {
+        source: '/*',
+        destination: 'https://supabase.com/docs/guides/auth/auth-helpers/auth-ui',
+        permanent: true,
+        basePath: false,
+      },
+    ]
+  },
+})
